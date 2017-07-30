a0985b8ffde7b33c042ec768a456ec0088b2f94f
connor <cjmccann@bu.edu>
2017-07-30 23:25:11 +0000

    working on C fcn for CPU load

--- c/cpu/cpu_load.c
+++ c/cpu/cpu_load.c
@@ -1,6 +1,24 @@
+//================================================================================
+/*  
+ *  Project : CPU Stats for RPI Cluster Network
+ *  Author  : Connor McCann
+ *  Date    : 01 Aug 2017
+ *  Source  : https://www.raspberrypi.org/forums/viewtopic.php?t=64835&p=479657
+ *  Source  : https://en.wikipedia.org/wiki/Load_%28computing%29#CPU_Load_Computation
+*/
+//================================================================================
+
 #include <mpi.h>
 #include <stdio.h>
 
+
 int main(int argc, char** argv) {
-  printf("This is the CPU Program\n");
+  FILE *fp;
+  char buff[255];
+
+  fp = fopen("/proc/stat", "r");
+  fgets(buff, 255, (FILE*)fp);
+  printf("%s\n", buff);
+  fclose(fp);
 }
+
--- c/runC.sh
+++ c/runC.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-program=$1
-
-echo "Running $program on cluster"
-
-mpiexec -f ../machinefile -n 4 $program
+if [ "$1" == "-h" ]; then 
+  echo "./runC.sh -p [./path/to/exec]"
+  exit 0
+elif [ "$1" != "" ]; then
+  program=$1
+  if [ -e $program ]; then
+    echo "Running $program on cluster"
+    echo "==========================="
+    mpiexec -f ../machinefile -n 4 $program
+  else
+    echo "Error: The file does not exist"
+    exit 1
+  fi
+fi
 
 # end
+
